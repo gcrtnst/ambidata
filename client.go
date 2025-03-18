e5ed9e74a9c80e94136253045b7937829c471d9f
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-03-19 00:07:21 +0900

    Add StatusCodeError

--- client.go
+++ client.go
@@ -34,3 +34,20 @@ func (err *APIError) Error() string {
 	b.WriteString(err.Err.Error())
 	return b.String()
 }
+
+type StatusCodeError struct {
+	StatusCode int
+}
+
+func (err *StatusCodeError) Error() string {
+	if err == nil || err.StatusCode == 0 {
+		return fmt.Sprintf("%#v", err)
+	}
+
+	code := err.StatusCode
+	text := http.StatusText(code)
+	if text == "" {
+		text = "Unknown Status Code"
+	}
+	return strconv.Itoa(code) + " " + text
+}
--- client_test.go
+++ client_test.go
@@ -46,6 +46,42 @@ func TestAPIErrorError(t *testing.T) {
 	}
 }
 
+func TestStatusCodeError(t *testing.T) {
+	tt := []struct {
+		name string
+		in   *StatusCodeError
+		want string
+	}{
+		{
+			name: "Nil",
+			in:   nil,
+			want: "(*ambidata.StatusCodeError)(nil)",
+		},
+		{
+			name: "EmptyStatusCode",
+			in:   &StatusCodeError{},
+			want: "&ambidata.StatusCodeError{StatusCode:0}",
+		},
+		{
+			name: "Normal",
+			in:   &StatusCodeError{StatusCode: http.StatusNotFound},
+			want: "404 Not Found",
+		},
+		{
+			name: "UnknownStatusCode",
+			in:   &StatusCodeError{StatusCode: 999},
+			want: "999 Unknown Status Code",
+		},
+	}
+
+	for _, tc := range tt {
+		got := tc.in.Error()
+		if got != tc.want {
+			t.Errorf("%s: expected %#v, got %#v", tc.name, tc.want, got)
+		}
+	}
+}
+
 type mockError struct{}
 
 func (err mockError) Error() string {
