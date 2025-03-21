91ceeed66869da1ed711c7983d04592cf932cd2d
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-03-21 23:24:58 +0900

    Add (*Fetcher).GetChannel

--- fetcher.go
+++ fetcher.go
@@ -10,3 +10,27 @@ type Fetcher struct {
 	Ch      string
 	Config  *Config
 }
+
+func (f *Fetcher) GetChannel(ctx context.Context) (ChannelInfo, error) {
+	path := "/api/v2/channels/" + url.PathEscape(f.Ch) + "/"
+	var j jsonRecvChannelInfo
+	err := f.httpGetJSON(ctx, path, nil, &j)
+	if err != nil {
+		return ChannelInfo{}, err
+	}
+
+	ret := j.ToChannelInfo()
+	return ret, nil
+}
+
+func (f *Fetcher) httpGetJSON(ctx context.Context, path string, query url.Values, v any) error {
+	const key = "readKey"
+	val := f.ReadKey
+	if query == nil {
+		query = url.Values{key: []string{val}}
+	} else {
+		query.Set(key, val)
+	}
+
+	return httpGetJSON(ctx, f.Config, path, query, v)
+}
