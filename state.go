4ab0283bb9d64392daf7ae0214188efeb1eb363e
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-04-11 00:22:26 +0900

    Add StateColorXXX

---
+++ state.go
@@ -0,0 +1,77 @@
+package ambidata
+
+const (
+	StateColorNone = iota
+
+	StateColorBlack      // #000000
+	StateColorDarkGrey3  // #444444
+	StateColorDarkGrey2  // #666666
+	StateColorDarkGrey1  // #999999
+	StateColorLightGrey1 // #cccccc
+	StateColorLightGrey2 // #eeeeee
+	StateColorLightGrey3 // #f3f3f3
+	StateColorWrite      // #ffffff
+
+	StateColorRed    // #ff0000
+	StateColorOrange // #ff9900
+	StateColorYellow // #ffff00
+	StateColorGreen  // #00ff00
+	StateColorCyan   // #00ffff
+	StateColorBlue   // #0000ff
+	StateColorPurple // #9900ff
+	StateColorPink   // #ff00ff
+
+	StateColorLightRed3    // #f4cccc
+	StateColorLightOrange3 // #fce5cd
+	StateColorLightYellow3 // #fff2cc
+	StateColorLightGreen3  // #d9ead3
+	StateColorLightCyan3   // #d0e0e3
+	StateColorLightBlue3   // #cfe2f3
+	StateColorLightPurple3 // #d9d2e9
+	StateColorLightPink3   // #ead1dc
+
+	StateColorLightRed2    // #ea9999
+	StateColorLightOrange2 // #f9cb9c
+	StateColorLightYellow2 // #ffe599
+	StateColorLightGreen2  // #b6d7a8
+	StateColorLightCyan2   // #a2c4c9
+	StateColorLightBlue2   // #9fc5e8
+	StateColorLightPurple2 // #b4a7d6
+	StateColorLightPink2   // #d5a6bd
+
+	StateColorLightRed1    // #e06666
+	StateColorLightOrange1 // #f6b26b
+	StateColorLightYellow1 // #ffd966
+	StateColorLightGreen1  // #93c47d
+	StateColorLightCyan1   // #76a5af
+	StateColorLightBlue1   // #6fa8dc
+	StateColorLightPurple1 // #8e7cc3
+	StateColorLightPink1   // #c27ba0
+
+	StateColorDarkRed1    // #cc0000
+	StateColorDarkOrange1 // #e69138
+	StateColorDarkYellow1 // #f1c232
+	StateColorDarkGreen1  // #6aa84f
+	StateColorDarkCyan1   // #45818e
+	StateColorDarkBlue1   // #3d85c6
+	StateColorDarkPurple1 // #674ea7
+	StateColorDarkPink1   // #a64d79
+
+	StateColorDarkRed2    // #990000
+	StateColorDarkOrange2 // #b45f06
+	StateColorDarkYellow2 // #bf9000
+	StateColorDarkGreen2  // #38761d
+	StateColorDarkCyan2   // #134f5c
+	StateColorDarkBlue2   // #0b5394
+	StateColorDarkPurple2 // #351c75
+	StateColorDarkPink2   // #741b47
+
+	StateColorDarkRed3    // #660000
+	StateColorDarkOrange3 // #783f04
+	StateColorDarkYellow3 // #7f6000
+	StateColorDarkGreen3  // #274e13
+	StateColorDarkCyan3   // #0c343d
+	StateColorDarkBlue3   // #073763
+	StateColorDarkPurple3 // #20124d
+	StateColorDarkPink3   // #4c1130
+)
